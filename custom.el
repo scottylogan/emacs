7fea8a15a4880e2770f59cebeeb6640503552d31
Scotty Logan <swl@stanford.edu>
2018-03-29 18:44:19 -0700

    more packages

--- custom.el
+++ custom.el
@@ -3,9 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(indent-tabs-mode nil)
+ '(js-indent-level 2)
+ '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (package-build shut-up epl commander f dash s ac-html ac-html-bootstrap ac-js2 ac-php ac-php-core apache-mode apt-sources-list cask cask-mode dayone terraform-mode twilight-anti-bright-theme twilight-theme x509-mode xcode-mode xcode-project xterm-color xterm-title yaml-mode docker dockerfile-mode egg feature-mode flymake-jshint flymake-jslint flymake-json flymake-puppet flymake-shell flymake-yaml fontawesome gist git git-timemachine gitattributes-mode gitconfig gitconfig-mode github-browse-file github-clone github-issues github-theme gitignore-mode gitlab go-mode google-this groovy-mode grunt hcl-mode homebrew-mode ini-mode iodine-theme ipcalc irfc jq-mode js-auto-beautify js-auto-format-mode js-doc js-format js-import js2-highlight-vars js2-refactor js3-mode json-navigator json-reformat jss jst kubernetes-evil kubernetes-tramp launchctl ldap-mode legalese lice logstash-conf markdown-mode+ markdown-preview-eww markdown-preview-mode markdown-toc mocha mocha-snippets mocker nodejs-repl npm-mode osx-browse osx-clipboard osx-lib osx-plist pbcopy pug-mode puppet-mode systemd))))
+    (vagrant markdown-mode package-build shut-up epl commander f dash s ac-html ac-html-bootstrap ac-js2 ac-php ac-php-core apache-mode apt-sources-list cask cask-mode dayone terraform-mode twilight-anti-bright-theme twilight-theme x509-mode xcode-mode xcode-project xterm-color xterm-title yaml-mode docker dockerfile-mode egg feature-mode flymake-jshint flymake-jslint flymake-json flymake-puppet flymake-shell flymake-yaml fontawesome gist git git-timemachine gitattributes-mode gitconfig gitconfig-mode github-browse-file github-clone github-issues github-theme gitignore-mode gitlab go-mode google-this groovy-mode grunt hcl-mode homebrew-mode ini-mode iodine-theme ipcalc irfc jq-mode js-auto-beautify js-auto-format-mode js-doc js-format js-import js2-highlight-vars js2-refactor js3-mode json-navigator json-reformat jss jst kubernetes-evil kubernetes-tramp launchctl ldap-mode legalese lice logstash-conf markdown-mode+ markdown-preview-eww markdown-preview-mode markdown-toc mocha mocha-snippets mocker nodejs-repl npm-mode osx-browse osx-clipboard osx-lib osx-plist pbcopy pug-mode puppet-mode systemd)))
+ '(sh-basic-offset 2)
+ '(sh-indentation 2)
+ '(size-indication-mode t)
+ '(tab-width 2)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
