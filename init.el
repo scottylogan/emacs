0c8c647f88af6a9fdb6611074dfdf8e46be24b6c
Scotty Logan <swl@stanford.edu>
2018-03-29 18:43:58 -0700

    new empty frame support; tab settings; server

--- init.el
+++ init.el
@@ -11,7 +11,24 @@
   )
 
 (setq custom-file "~/.emacs.d/custom.el")
+
+;; based on https://stackoverflow.com/questions/25791605/emacs-how-do-i-create-a-new-empty-buffer-whenever-creating-a-new-frame
+
+(defun scotty-new-empty-frame ()
+  "Create a new frame with a new empty buffer."
+  (interactive)
+  (let ((buffer (generate-new-buffer "untitled")))
+    (set-buffer-major-mode buffer)
+    (display-buffer buffer '(display-buffer-pop-up-frame . nil))))
+
+(global-set-key (kbd "s-n") #'scotty-new-empty-frame)
+(global-set-key (kbd "s-r") #'replace-string)
+
+(setq tab-width 2) ; or any other preferred value
+(defvaralias 'c-basic-offset 'tab-width)
+(defvaralias 'cperl-indent-level 'tab-width)
 ;; keep this last
 (load custom-file)
 
+(server-start)
 
