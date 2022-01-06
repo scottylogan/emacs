17ad3710b0a4a1b0d01acdb59ffa7b83e7909aec
Scotty Logan <swl@stanford.edu>
2022-01-05 18:03:32 -0800

    updated packages and .gitignore

--- custom.el
+++ custom.el
@@ -5,13 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(apache-indent-level 2)
  '(column-number-mode t)
+ '(exec-path
+   '("~/bin/" "/usr/local/bin" "/usr/bin" "/bin" "/usr/local/sbin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_14" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_14" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin"))
  '(freeradius-indent-offset 2)
  '(indent-tabs-mode nil)
  '(js-indent-level 2)
  '(menu-bar-mode nil)
  '(package-selected-packages
-   (quote
-    (ansi composer flymake-php flymake-phpcs php-refactor-mode phpunit vagrant markdown-mode package-build shut-up epl commander f dash s ac-html ac-html-bootstrap ac-js2 ac-php ac-php-core apache-mode apt-sources-list cask cask-mode dayone terraform-mode twilight-anti-bright-theme twilight-theme x509-mode xcode-mode xcode-project xterm-color xterm-title yaml-mode docker dockerfile-mode egg feature-mode flymake-jshint flymake-jslint flymake-json flymake-puppet flymake-shell flymake-yaml fontawesome freeradius-mode gist git git-timemachine gitattributes-mode gitconfig gitconfig-mode github-browse-file github-clone github-issues github-theme gitignore-mode gitlab go-mode google-this groovy-mode grunt hcl-mode homebrew-mode ini-mode iodine-theme ipcalc irfc jq-mode js-auto-beautify js-auto-format-mode js-doc js-format js-import js2-highlight-vars js2-refactor js3-mode json-navigator json-reformat jss jst kubernetes-tramp launchctl ldap-mode legalese lice logstash-conf markdown-mode+ markdown-preview-eww markdown-preview-mode markdown-toc mocha mocha-snippets mocker nodejs-repl npm-mode osx-browse osx-clipboard osx-lib osx-plist pbcopy php-mode pug-mode puppet-mode systemd)))
+   '(fold-this ansi composer flymake-php flymake-phpcs php-refactor-mode phpunit vagrant markdown-mode package-build shut-up epl commander f dash s ac-html ac-html-bootstrap ac-js2 ac-php ac-php-core apache-mode apt-sources-list cask cask-mode dayone terraform-mode twilight-anti-bright-theme twilight-theme x509-mode xcode-mode xcode-project xterm-color xterm-title yaml-mode docker dockerfile-mode egg feature-mode flymake-jshint flymake-jslint flymake-json flymake-puppet flymake-shell flymake-yaml fontawesome freeradius-mode gist git git-timemachine gitattributes-mode gitconfig gitconfig-mode github-browse-file github-clone github-issues github-theme gitignore-mode gitlab go-mode google-this groovy-mode grunt hcl-mode homebrew-mode ini-mode iodine-theme ipcalc irfc jq-mode js-auto-beautify js-auto-format-mode js-doc js-format js-import js2-highlight-vars js2-refactor js3-mode json-navigator json-reformat jss jst kubernetes-tramp launchctl ldap-mode legalese lice logstash-conf markdown-mode+ markdown-preview-eww markdown-preview-mode markdown-toc mocha mocha-snippets mocker nodejs-repl npm-mode osx-browse osx-clipboard osx-lib osx-plist pbcopy php-mode pug-mode puppet-mode systemd))
  '(sh-basic-offset 2)
  '(sh-indentation 2)
  '(size-indication-mode t)
