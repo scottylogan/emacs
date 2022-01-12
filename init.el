9116f102a09672a68f7e7f0769e8ce2e068fa8a0
Scotty Logan <swl@stanford.edu>
2022-01-12 13:23:32 -0800

    added org-mode setup

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (setq inhibit-splash-screen t)
 
+(setq frame-title-format "%f")
+
 (defun scotty-frame-setup (&optional frame)
   "Configure new frames."
   (when (and (eq system-type 'darwin) (eq window-system 'ns))
@@ -66,4 +68,40 @@
              (server-running-p))
   (server-start))
 
+
+
 (put 'downcase-region 'disabled nil)
+
+;;; org-mode setup
+;;; https://orgmode.org/worg/org-tutorials/orgtutorial_dto.html
+;;; http://pragmaticemacs.com/emacs/org-mode-basics-vi-a-simple-todo-list/
+;;; http://doc.norang.ca/org-mode.html
+
+;; set key for agenda
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+;; file to save todo items
+(setq default-agenda-dir "~/Dropbox/org-mode")
+(setq org-agenda-files (list default-agenda-dir))
+(setq default-agenda-file
+      (concat (file-name-as-directory default-agenda-dir) "todo.org"))
+
+
+;; set priority range from A to C with default A
+(setq org-highest-priority ?A)
+(setq org-lowest-priority ?C)
+(setq org-default-priority ?B)
+
+;;set colours for priorities
+(setq org-priority-faces '((?A . (:foreground "#F0DFAF" :weight bold))
+                           (?B . (:foreground "LightSteelBlue"))
+                           (?C . (:foreground "OliveDrab"))))
+
+;;open agenda in current window
+(setq org-agenda-window-setup (quote current-window))
+
+;;capture todo items using C-c c t
+(define-key global-map (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      '(("t" "todo" entry (file+headline default-agenda-file "Tasks")
+         "* TODO [#A] %?")))
