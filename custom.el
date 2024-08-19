1bb2298ce88d7da6701de6a262acc9d19399f043
Scotty Logan <swl@stanford.edu>
2024-08-19 11:59:09 -0700

    updates

--- custom.el
+++ custom.el
@@ -3,10 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   '(yaml ac-html ac-html-bootstrap ac-js2 ac-php ac-php-core ansi apache-mode apt-sources-list cask cask-mode commander composer dash dayone docker dockerfile-mode egg epl f feature-mode flymake-jshint flymake-jslint flymake-json flymake-php flymake-phpcs flymake-puppet flymake-shell flymake-yaml fold-this fontawesome freeradius-mode gist git git-timemachine gitattributes-mode gitconfig gitconfig-mode github-browse-file github-clone github-issues github-theme gitignore-mode gitlab go-mode google-this groovy-mode grunt hcl-mode homebrew-mode ini-mode iodine-theme ipcalc irfc jq-mode js-auto-beautify js-auto-format-mode js-doc js-format js-import js2-highlight-vars js2-refactor js3-mode json-navigator json-reformat jss jst kubernetes-tramp launchctl ldap-mode legalese lice logstash-conf markdown-mode markdown-mode+ markdown-preview-eww markdown-preview-mode markdown-toc mocha mocha-snippets mocker nodejs-repl npm-mode osx-browse osx-clipboard osx-lib osx-plist package-build pbcopy php-mode php-refactor-mode phpunit pug-mode puppet-mode s shut-up systemd terraform-mode twilight-anti-bright-theme twilight-theme vagrant x509-mode xcode-mode xcode-project xterm-color xterm-title yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 220 :width normal :foundry "nil" :family "Source Code Pro")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight medium :height 220 :width normal :foundry "nil" :family "Droid Sans Mono Dotted")))))
+ 
